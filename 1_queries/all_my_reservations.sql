e915be3f850849c1e4d0ca58ee351939a8862755
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-06-01 23:47:05 -0700

    lightbnb boilerplate

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -4,7 +4,8 @@ avg(property_reviews.rating) as average_rating
 FROM properties JOIN reservations ON reservations.property_id = properties.id
 JOIN property_reviews ON property_reviews.property_id = properties.id
 JOIN users ON properties.owner_id = users.id
-WHERE users.email = 'tristanjacobs@gmail.com'
+WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.title, 
 reservations.start_date, properties.cost_per_night
+ORDER BY reservations.start_date
 LIMIT 10;
